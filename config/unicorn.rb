a4df568d8929ea35e8d1726cedf92b5b28adefbd
Martin Honermeyer <martin.honermeyer@zweitag.de>
2014-03-13 00:47:43 +0100

    Configure unicorn log file

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,10 @@
 worker_processes 3
 timeout 30
 preload_app true
+
 listen File.join(Dir.pwd, 'tmp/sockets/unicorn.sock')
 pid File.join(Dir.pwd, 'tmp/pids/unicorn.pid')
+
+unicorn_log_path = File.join(Dir.pwd, 'log/unicorn.log')
+stderr_path unicorn_log_path
+stdout_path unicorn_log_path
